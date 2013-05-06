633e43d6edd4f2f6099cabf2cad377349c78fe24
Olof Olsson <borsna@gmail.com>
2013-05-06 10:31:11 +0200

    added remove statement for RDF/XML-ABBREV in build-script
    Also added N3 and XML versions of the turtle file.

--- build/build.bat
+++ build/build.bat
@@ -2,10 +2,15 @@
 SET SAXON=C:\Users\olof\Dropbox\utils\saxon-he\saxon9he.jar
 
 echo -----convert turtle to other formats-----
+echo RDF/XML-ABBREV
 call rdfcopy ../discovery.ttl TURTLE RDF/XML-ABBREV > ../discovery-abbrev.xml
+echo RDF/XML
 call rdfcopy ../discovery.ttl TURTLE RDF/XML > ../discovery.xml
+echo N3
 call rdfcopy ../discovery.ttl TURTLE N3 > ../discovery.n3
 
 echo -----create html documentation from rdf/xml-----
 call java -cp %SAXON% net.sf.saxon.Transform -t -s:../discovery-abbrev.xml -xsl:rdf2html.xsl -o:../documentation/classes.html
+echo removing rdf/xml abbrev (used for generation the documentation)
+del ..\discovery-abbrev.xml
 echo -----done-----
--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,15 @@
 ﻿#!/bin/sh
 SAXON=path/to/saxon-he/
 echo convert turtle to other formats
+echo RDF/XML-ABBREV
 rdfcopy ../discovery.ttl TURTLE RDF/XML-ABBREV > ../discovery-abbrev.xml
+echo RDF/XML
 rdfcopy ../discovery.ttl TURTLE RDF/XML > ../discovery.xml
+echo N3
 rdfcopy ../discovery.ttl TURTLE N3 > ../discovery.n3
 
 echo create html documentation from rdf/xml
 java -cp $SAXON net.sf.saxon.Transform -t -s:../discovery-abbrev.xml -xsl:rdf2html.xsl -o:../documentation/classes.html
+echo removing rdf/xml abbrev (used for generation the documentation)
+rm ../discovery-abbrev.xml
 echo done
