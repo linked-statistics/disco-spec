2accc4105befc3f58604b7edece74c07bdeaf351
Olof Olsson <olof@debian70.vm>
2014-04-19 11:25:27 +0000

    render full table

--- build/mappings/csv2html.sh
+++ build/mappings/csv2html.sh
@@ -7,14 +7,30 @@ INPUT=$1
 OLDIFS=$IFS
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+
+echo '<table style="text-align: left; font-size: 70%;">'
+echo '	<thead>'
+echo '	  <tr>'
+echo '	    <th style="vertical-align: top; text-align: center;">Entity</th>'
+echo '		<th style="vertical-align: top; text-align: center;">DDI-C</td>'
+echo '		<th style="vertical-align: top; text-align: center;">DDI-L</th>'
+echo '		<th style="vertical-align: top; text-align: center;">Context</th>'
+echo '		<th style="vertical-align: top; text-align: center;">SPARQL</th>'
+echo '	    </tr>'
+echo '  </thead>'
+echo '  <tbody>'
+
 sed 1d $INPUT | while read entity ddi_c ddi_l context sparql
 do
-	echo "<tr>"
-	echo "  <td>$entity</td>"
-	echo "  <td>$ddi_c</td>"
-	echo "  <td>$ddi_l</td>"
-	echo "  <td>$context</td>"
-	echo "  <td>$sparql</td>"
-	echo "</tr>"
+	echo "    <tr>"
+	echo "      <td>$entity</td>"
+	echo "      <td>$ddi_c</td>"
+	echo "      <td>$ddi_l</td>"
+	echo "      <td>$context</td>"
+	echo "      <td>$sparql</td>"
+	echo "    </tr>"
 done
+
+echo '  </tbody>'
+echo '</table>'
 IFS=$OLDIFS
