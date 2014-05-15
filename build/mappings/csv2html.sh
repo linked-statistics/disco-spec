b03ec00ec57b4b29b827505ef16adecdea58592d
Olof Olsson <borsna@gmail.com>
2014-05-15 11:01:33 +0200

    script ignores DDI-C mappings

--- build/mappings/csv2html.sh
+++ build/mappings/csv2html.sh
@@ -9,26 +9,30 @@ IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 
 echo '<table style="text-align: left; font-size: 70%;">'
-echo '	<thead>'
-echo '	  <tr>'
-echo '	    <th style="vertical-align: top; text-align: center;">Entity</th>'
-echo '		<th style="vertical-align: top; text-align: center;">DDI-C</td>'
-echo '		<th style="vertical-align: top; text-align: center;">DDI-L</th>'
-echo '		<th style="vertical-align: top; text-align: center;">Context</th>'
-echo '		<th style="vertical-align: top; text-align: center;">SPARQL</th>'
-echo '	    </tr>'
+echo '<thead>'
+echo '      <tr>'
+echo '          <td style="font-weight: bold;">#<br/></td>'
+echo '          <td style="vertical-align: top; text-align: center; font-weight: bold;">property<br/></td>'
+echo '          <td style="vertical-align: top; text-align: center; font-weight: bold;">domain class<br/></td>'
+echo '          <td style="vertical-align: top; text-align: center; font-weight: bold;">range class<br/></td>'
+#echo '          <td style="vertical-align: top; text-align: center; font-weight: bold;">DDI-C<br/></td>'
+echo '          <td style="vertical-align: top; text-align: center; font-weight: bold;">DDI-L<br/></td>'
+echo '      </tr>'
 echo '  </thead>'
 echo '  <tbody>'
 
+COUNTER=1
 sed 1d $INPUT | while read entity ddi_c ddi_l context sparql
 do
 	echo "    <tr>"
+	echo "      <td>#$COUNTER</td>"
 	echo "      <td>$entity</td>"
-	echo "      <td>$ddi_c</td>"
+#	echo "      <td>$ddi_c</td>"
 	echo "      <td>$ddi_l</td>"
 	echo "      <td>$context</td>"
 	echo "      <td>$sparql</td>"
 	echo "    </tr>"
+	let COUNTER=COUNTER+1
 done
 
 echo '  </tbody>'
